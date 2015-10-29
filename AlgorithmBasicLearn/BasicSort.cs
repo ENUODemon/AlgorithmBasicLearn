5aaa5ca1827c212795cc13db8199a091a5aeb9f3
ENUODemon <552448713@qq.com>
2015-10-29 16:24:05 +0800

    add basic aligorithm

---
+++ AlgorithmBasicLearn/BasicSort.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmBasicLearn
+{
+    public static class BasicSort
+    {
+        //冒泡排序,注释部分为改进算法
+        public static int[] BubbleSort(int[] lists)
+        {
+
+            int length = lists.Length;
+            int temp = 0;
+            for (int i = 0; i < length; i++)
+            {
+                // bool flag = false;
+                for (int j = length - 1; j > 0; j--)
+                {
+                    if (lists[j] < lists[j - 1])
+                    {
+                        temp = lists[j - 1];
+                        lists[j - 1] = lists[j];
+                        lists[j] = temp;
+
+                        //    flag = true;
+                    }
+                }
+                //if (flag == false)
+                //{
+                //    return lists;
+                //}
+            }
+
+            return lists;
+        }
+
+        //简单选择排序
+        public static int[] SelectSort(int[] a)
+        {
+            int n = a.Length;
+            int min = 0;
+            int temp = 0;
+            for (int i = 0; i < n; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (a[j] < a[min])
+                    {
+                        temp = a[min];
+                        a[min] = a[j];
+                        a[j] = temp;
+                    }
+                }
+
+            }
+            return a;
+
+        }
+        //直接插入排序
+        public static int[] StrainghtInsertSort(int[] a)
+        {
+            int n = a.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int temp = a[i];
+                int j = 0;
+                for (j = i - 1; a[j] > temp; j--)
+                { 
+                    a[j + 1] = a[j];
+                }
+                a[j + 1] = temp;
+            }
+
+            return a;
+        }
+
+        private static void swap(int[] arr, int pos, int offset)
+        {
+            int temp = arr[pos];
+            arr[pos] = arr[offset];
+            arr[offset] = temp;
+        }
+    }
+}
---
+++ AlgorithmBasicLearn/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmBasicLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lists = { 2, 66, 53, 54, 52, 76, 42, 33, 18, 99 };
+            lists = BasicSort.StrainghtInsertSort(lists);
+
+            foreach (var l in lists)
+            {
+                Console.WriteLine(l);
+            }
+            Console.Read();
+        }
+
+    }
+}
