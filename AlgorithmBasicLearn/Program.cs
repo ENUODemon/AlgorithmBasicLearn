0a32681369717f3263beccae10fa1fa164f62c0a
ENUODemon <552448713@qq.com>
2017-03-31 15:44:55 +0800

    comit 2

--- AlgorithmBasicLearn/Program.cs
+++ AlgorithmBasicLearn/Program.cs
@@ -12,8 +12,6 @@ namespace AlgorithmBasicLearn
         {
             int[] lists = { 2, 66, 53, 54, 52, 76, 42, 33, 18, 99 };
             //lists = BasicSort.MergeSort(lists, lists.Count());
-
-
             foreach (var l in lists)
             {
                 Console.WriteLine(l);
